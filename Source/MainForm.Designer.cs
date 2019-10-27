977cdf0b1f3f658d23f28532aa010556278b3a0e
helloing.world <dev@helloing.world>
2019-10-27 19:27:01 +0200

    First commit

---
+++ Source/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace NELOLF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filesListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            // 
+            // filesListView
+            // 
+            this.filesListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.filesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.filesListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filesListView.FullRowSelect = true;
+            this.filesListView.HideSelection = false;
+            this.filesListView.HoverSelection = true;
+            this.filesListView.Location = new System.Drawing.Point(16, 16);
+            this.filesListView.MultiSelect = false;
+            this.filesListView.Name = "filesListView";
+            this.filesListView.Size = new System.Drawing.Size(1162, 587);
+            this.filesListView.TabIndex = 0;
+            this.filesListView.UseCompatibleStateImageBehavior = false;
+            this.filesListView.View = System.Windows.Forms.View.Details;
+            this.filesListView.VirtualMode = true;
+            this.filesListView.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.filesListView_RetrieveVirtualItem);
+            this.filesListView.SizeChanged += new System.EventHandler(this.filesListView_SizeChanged);
+            this.filesListView.DoubleClick += new System.EventHandler(this.filesListView_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Item path";
+            this.columnHeader1.Width = 1100;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1178, 619);
+            this.Controls.Add(this.filesListView);
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(16, 16, 0, 16);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NELOLF";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView filesListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Source/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NELOLF
+{
+    public partial class MainForm : Form
+    {
+        private MenuItem drivesMenuItem;
+        private List<string> filesList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            filesList = new List<string>();
+
+            addMainMenu();
+            SetWindowTheme(this.filesListView.Handle, "Explorer", null);
+
+        }
+
+
+        private void findAddFiles(string drive)
+        {
+            this.Text = "NELOLF - Scanning...";
+            this.drivesMenuItem.Enabled = false;
+            this.filesListView.VirtualListSize = 0;
+            this.filesList.Clear();
+            
+            //ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe", "/c dir /b /s x:\\")
+            //{
+            //    //WorkingDirectory = "t:\\",
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true,
+            //    RedirectStandardOutput = true
+            //};
+
+            Process process = new Process();
+            //process.StartInfo = startInfo;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = $"/c dir /b /s {drive}";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            //process.StartInfo.WorkingDirectory = "T:\\";
+            process.EnableRaisingEvents = true;
+            process.SynchronizingObject = this;
+            process.Exited += new EventHandler(Process_Exited);
+
+            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                //Debug.WriteLine(e.Data);
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                    filesList.Add(e.Data);
+            });
+
+            process.Start();
+            process.BeginOutputReadLine();
+            //process.WaitForExit();
+
+            
+        }
+
+        private void Process_Exited(object sender, System.EventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                this.filesListView.VirtualListSize = this.filesList.Count;
+                this.Text = $"NELOLF - {this.filesList.Count:n0} files";
+                this.drivesMenuItem.Enabled = true;
+                //this.filesListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                //this.filesListView.Columns[0].Width = -1;
+            }));
+        }
+
+        private void filesListView_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = new ListViewItem(this.filesList[e.ItemIndex]);
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            //this.findAddFiles("C:\\");
+        }
+
+        private void addMainMenu()
+        {
+
+            this.Menu = new MainMenu();
+            MenuItem fileMenu = new MenuItem("&File");
+            this.Menu.MenuItems.Add(fileMenu);
+            fileMenu.MenuItems.Add(new MenuItem("E&xit", (o, e) => { this.Close();  }));
+
+            this.drivesMenuItem = new MenuItem("&Drives");
+            this.Menu.MenuItems.Add(this.drivesMenuItem);
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                MenuItem driveMenuItem = new MenuItem(d.Name);
+                driveMenuItem.Click += DriveMenuItem_Click;
+                this.drivesMenuItem.MenuItems.Add(driveMenuItem);
+            }
+
+            MenuItem helpMenu = new MenuItem("&Help");
+            this.Menu.MenuItems.Add(helpMenu);
+        }
+
+        private void DriveMenuItem_Click(object sender, EventArgs e)
+        {
+            this.findAddFiles((sender as MenuItem).Text);
+        }
+
+        [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int SetWindowTheme(IntPtr hwnd, string pszSubAppName, string pszSubIdList);
+
+        private void filesListView_SizeChanged(object sender, EventArgs e)
+        {
+            //this.filesListView.Columns[0].Width = this.filesListView.Width - 100;
+        }
+
+        /// <summary>
+        /// Starts a new process and opens the location of the specified file in Explorer and selects it.
+        /// </summary>
+        /// <param name="fileName">The name of the file to be opened.</param>
+        /// <returns>The process object that was used to open the file.</returns>
+        public static Process Explore(string fileName)
+        {
+            return (!string.IsNullOrEmpty(fileName)) ?
+                Process.Start("explorer.exe", "/select,\"" + fileName + "\"") : null;
+        }
+
+        private void filesListView_DoubleClick(object sender, EventArgs e)
+        {
+            Explore(this.filesList[this.filesListView.SelectedIndices[0]]);
+        }
+    }
+}
